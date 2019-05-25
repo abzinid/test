629c7cae4ddd904cd47570ba79a1d1a8c6662f14
abzinid <45764160+abzinid@users.noreply.github.com>
2019-05-25 12:19:06 +0200

    Create redirect-test.js

---
+++ redirect-test.js
@@ -0,0 +1,16 @@
+if(window.location.pathname == "/join?m=1")
+{
+window.location.assign("https://www.google.com");
+}
+if(window.location.pathname == "/join")
+{
+window.location.assign("https://www.google.com");
+}
+if(window.location.pathname == "/lates?m=1")
+{
+window.location.assign("https://www.google.com");
+}
+if(window.location.pathname == "/latest")
+{
+window.location.assign("https://www.google.com");
+}
