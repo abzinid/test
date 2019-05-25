f6b143b46bed7194643b949ff0c42f4df25e3e15
abzinid <45764160+abzinid@users.noreply.github.com>
2019-05-25 12:29:16 +0200

    Update and rename redirect-test.js to redirect-test2.js

---
+++ redirect-test2.js
@@ -0,0 +1,16 @@
+if(window.location.pathname == "/bing?m=1")
+{
+window.location.assign("https://www.bing.com");
+}
+if(window.location.pathname == "/bing")
+{
+window.location.assign("https://www.bing.com");
+}
+if(window.location.pathname == "/dropbox?m=1")
+{
+window.location.assign("dropbox.com");
+}
+if(window.location.pathname == "/dropbox")
+{
+window.location.assign("https://www.dropbox.com");
+}
